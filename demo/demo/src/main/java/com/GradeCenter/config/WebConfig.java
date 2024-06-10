0a95a274e9013ca36742ffa9dd09b8c6bdd214ff
Penchev <36088861+APPenchev@users.noreply.github.com>
2024-06-10 20:00:47 +0300

    Parent Service Layer set up

--- demo/demo/src/main/java/com/GradeCenter/config/WebConfig.java
+++ demo/demo/src/main/java/com/GradeCenter/config/WebConfig.java
@@ -8,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply CORS to all paths
-                .allowedOrigins("http://localhost:5173") // Allow this origin
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
